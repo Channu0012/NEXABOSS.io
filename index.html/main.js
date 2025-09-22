c2f33ef9782a7acf2af089e341cc4e83dc352a1e
Channu0012 <channupatil299@gmail.com>
2025-09-22 16:26:11 +0530

    Add files via upload

---
+++ index.html/main.js
@@ -0,0 +1,47 @@
+// Smooth scroll for nav links
+const navLinks = document.querySelectorAll('.main-header nav a');
+navLinks.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const target = document.querySelector(link.getAttribute('href'));
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+
+// Loader Hide
+window.addEventListener('load', () => {
+  const loader = document.getElementById('loader');
+  loader.style.display = 'none';
+});
+
+//slides bar section.....
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
